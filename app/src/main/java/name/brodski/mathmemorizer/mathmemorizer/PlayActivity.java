91bfcf38623065d56b22add092af2442488db89a
Boris Brodski <brodsky_boris@yahoo.com>
2016-09-22 23:11:42 +0200

    Simple activities

---
+++ app/src/main/java/name/brodski/mathmemorizer/mathmemorizer/PlayActivity.java
@@ -0,0 +1,43 @@
+package name.brodski.mathmemorizer.mathmemorizer;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class PlayActivity extends AppCompatActivity implements PlayMultipleChoiceFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+/*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+*/
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        PlayMultipleChoiceFragment fragment = PlayMultipleChoiceFragment.newInstance(new CharSequence[]{"1", "2", "3", "4"});
+
+        fragment.setArguments(getIntent().getExtras());
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_choice, fragment).commit();
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
